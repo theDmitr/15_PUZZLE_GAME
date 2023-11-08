5e5063d80fcf5e2b1c8c5726c5f60d7c8c9ab0ff
theDmitr <dmitryfirson@gmail.com>
2023-11-08 23:07:21 +0300

    trash

--- GameEngine.cpp
+++
@@ -1,7 +0,0 @@
-#include "GameEngine.h"
-
-void GameEngine::init(Window* window, PuzzleGame* game)
-{
-	this->window = window;
-	this->game = game;
-}
--- Renderer.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "PuzzleGame.h"
-
-class Renderer
-{
-public:
-	void render(PuzzleGame game, RenderTarget& target, RenderStates states);
-};
-
